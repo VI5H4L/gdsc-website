9044721313f95bef74c71cfca757a2bfea908707
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2023-08-13 10:41:15 +0530

    Update Login.js

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import styles from './Login.module.css'; // If using CSS modules
+import axios from 'axios';
 
 const LoginForm = () => {
+
+    const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    try {
+      const response = await axios.post('http://localhost:8000/authentication/login', { email, password }); // Replace '/login' with your actual backend API endpoint
+      if (response.data.success) {
+        // Authentication successful, redirect to admin page
+        console.log('Login successful:', response.data.authtoken);
+        localStorage.setItem('authtoken', response.data.authtoken);
+        window.location.href = 'http://localhost:3000/admin'; 
+      } else {
+        setError('Invalid email or password');
+      }
+    } catch (error) {
+      setError('An error occurred during login');
+    }
+  };
+
+  
   return (
     <div className={styles.main}>
-    <form className={styles.loginForm}>
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
       <div className={styles.formgroup}>
         <label htmlFor="email">Email address</label>
-        <input type="email" className={styles.formcontrol} id="email" placeholder="Enter email" />
+        <input type="email" className={styles.formcontrol} id="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
       </div>
       <div className={styles.formgroup}>
         <label htmlFor="password">Password</label>
-        <input type="password" className={styles.formcontrol} id="password" placeholder="Password" />
+        <input type="password" className={styles.formcontrol} id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
+      {error && <p className={styles.error}>{error}</p>}
       <button type="submit" className={styles.btn}>Submit</button>
     </form>
     </div>
